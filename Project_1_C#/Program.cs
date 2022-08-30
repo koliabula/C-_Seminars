bd06cc29195df6e7d0f72bfc90a0970657d2f063
Bulla Kolia <koliakolan@gmail.com>
2022-08-30 11:12:34 +0300

    Project_1_C#

---
+++ Project_1_C#/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Введите число:_ ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень:_ ");
+int pow = Convert.ToInt32(Console.ReadLine());
+Console.Write(Math.Pow(number, pow) );
+
+
