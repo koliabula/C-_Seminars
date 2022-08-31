a74c73def85ffbc2e097aa87c848f5cb766403f8
Bulla Kolia <koliakolan@gmail.com>
2022-08-31 08:47:00 +0300

    Project_2_C#

---
+++ Project_2_C#/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число 1 :_ ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2 :_ ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 == number2 * number2){
+    Console.WriteLine("Да");
+}
+
+else {
+    Console.WriteLine("Нет");
+}
+
+
