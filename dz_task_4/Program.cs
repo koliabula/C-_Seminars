722f99d697cc1ab1d306732d1c51ad2dc5630db9
Bulla Kolia <koliakolan@gmail.com>
2022-08-31 09:40:09 +0300

    ДЗ Задача 4

---
+++ dz_task_4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число 1 :_ ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2 :_ ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 3 :_ ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+
+if (number2 > max){    
+    max = number2; 
+}
+
+if (number3 > max){    
+    max = number3; 
+}
+
+Console.WriteLine(max + " - Максимальное число");
