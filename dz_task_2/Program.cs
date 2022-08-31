116c89ce69b5a748cbc8814f0966254cae926e3f
Bulla Kolia <koliakolan@gmail.com>
2022-08-31 09:30:27 +0300

    ДЗ Задача 2

---
+++ dz_task_2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число 1 :_ ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2 :_ ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2){
+    Console.WriteLine($"Число {number1} Больше числа " +number2 );
+}
+
+if (number1 < number2){
+    Console.WriteLine($"Число {number2} Больше числа " + number1 );
+}
+
+if (number1 == number2){
+    Console.WriteLine($"Число {number1} равно числу " +  number1 );
+}
+
