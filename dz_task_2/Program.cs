cb989145f732d3b65f1029a4767a33ef5494c905
Bulla Kolia <koliakolan@gmail.com>
2022-08-31 09:43:23 +0300

    ДЗ Задача 2 с условием

--- dz_task_2/Program.cs
+++ dz_task_2/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.Write("Введите число 1 :_ ");
+﻿/*Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
+какое число большее, а какое меньшее.*/
+
+Console.Write("Введите число 1 :_ ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число 2 :_ ");
 int number2 = Convert.ToInt32(Console.ReadLine());
